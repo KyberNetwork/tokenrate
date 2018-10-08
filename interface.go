28ac913cbf807adfeee72b0dca9b1124783d026a
Diep Pham <diep@kyber.network>
2018-10-08 17:29:58 +0700

    add experimental note to Provider interface

--- interface.go
+++ interface.go
@@ -4,6 +4,8 @@ import "time"
 
 // Provider is the common interface to query historical rates of any
 // token to real worldp currencies.
+// **Experimental**: the token, each provider intepretered currency,
+// parameters differently.
 type Provider interface {
 	Rate(token, currency string, timestamp time.Time) (float64, error)
 }
